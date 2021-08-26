4d00cdfa94d64ef5344ffb861747f225717cd8b6
John Lollar <jlollar45@gmail.com>
2021-08-26 00:54:45 -0500

    Refactored into MVVM

---
+++ Barcode/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BarcodeScannerViewModel.swift
+//  Barcode
+//
+//  Created by ParkingPal on 8/26/21.
+//
+
+import Foundation
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not yet scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
