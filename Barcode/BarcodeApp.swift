6360518592d0ae25d0fbf21175b2d958963a38b8
John Lollar <jlollar45@gmail.com>
2021-08-25 22:49:11 -0500

    Initial Commit

---
+++ Barcode/BarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeApp.swift
+//  Barcode
+//
+//  Created by ParkingPal on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
